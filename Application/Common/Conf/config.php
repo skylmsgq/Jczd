b8199a076a3df6affdf1e29d099e902e217a16d4
kaiYuan Shi <skylmsgq@sjtu.edu.cn>
2014-11-09 09:57:33 +0800

    done

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => '127.0.0.1',
+	'DB_NAME' => 'test',
+	'DB_USER' => 'root',
+	'DB_PWD' => '',
+	'DB_PORT' => '3306',
+	'DB_PREFIX' => 'ea_',
+	'SITE_TITLE' => 'Jczd',
+	'SITE_KEYWORD' => 'Jczd',
+	'SITE_DESCRIPTION' => 'Jczd',
+	'LAYOUT_ON'=>true,
+	'LAYOUT_NAME'=>'layout',
+);
---
+++ Common/Conf/system_config.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => '127.0.0.1',
+  'DB_NAME' => 'test',
+  'DB_USER' => 'root',
+  'DB_PWD' => '',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'ea_',
+  'SITE_TITLE' => 'test',
+  'SITE_KEYWORD' => 'test',
+  'SITE_DESCRIPTION' => 'test',
+);
